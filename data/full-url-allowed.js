028016be3f8aad496e74d53b73c0de61b26a97d4
Li Xianghao <li_xianghao@YiMac.local>
2020-08-19 13:57:10 +0900

    Updated

--- data/full-url-allowed.js
+++ data/full-url-allowed.js
@@ -2,9 +2,9 @@ const full_url_allowed = [
   // {
   //   "url": "xxxxxxxx"
   // },
-  // {
-  //   "url": "xxxxxxxx"
-  // },
+  {
+    "url": "demo.wp-talk.com/content/intro/01/"
+  },
   {
     "url": "store.wp-talk.com/demo/k-theme/"
   }
